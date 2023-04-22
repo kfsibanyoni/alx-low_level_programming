5ddaed0176932f182aec11fbb9c2621f6213c262
kfsibanyoni <kfsibanyoni@gmail.com>
2023-04-22 03:29:57 -0700

    added an before int

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,7 +16,7 @@ int main(void)
 
 	/* sizeof() used to evaluate size of var. */
 	printf("Size of a char: %u byte(s)\n", sizeof(char_));
-	printf("Size of a int: %u byte(s)\n", sizeof(int_));
+	printf("Size of an int: %u byte(s)\n", sizeof(int_));
 	printf("Size of a long int: %u byte(s)\n", sizeof(longInt));
 	printf("Size of a long long int: %u byte(s)\n", sizeof(longLongInt));
 	printf("Size of a float: %u byte(s)\n", sizeof(float_));
