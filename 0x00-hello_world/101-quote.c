bb871fbe1dce9272ddc553efcdd640ead5f88643
kfsibanyoni <kfsibanyoni@gmail.com>
2023-04-22 03:47:40 -0700

    Will echo work?

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**                                                                                                                                                  
+ * main - with proper grammar, but the outcome is a piece of art,                                                                                    
+ *                                                                                                                                                   
+ * Return: 1                                                                                                                                  
+ */
+int main()
+{
+        echo "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+        return (1);
+}
