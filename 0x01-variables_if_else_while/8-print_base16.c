427e6ecd7868ceac4b144a61b9e7307cf7bd0939
kfsibanyoni <kfsibanyoni@gmail.com>
2023-04-24 16:02:37 -0700

    8. hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - main code block
+ *
+ * return: 0 always
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10, i++)
+	{
+		putchar(i);
+	}
+	for (i = 'a', i <= 'f', i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
