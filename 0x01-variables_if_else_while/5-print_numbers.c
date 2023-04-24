857cb2f6f1cec1debcb226f698bba1753f5c4a68
kfsibanyoni <kfsibanyoni@gmail.com>
2023-04-24 16:47:22 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - main code block
+ *
+ * Return: 0 always
+ *
+ */
+int main(void)
+{
+        int x;
+
+        for (x = 0; x < 10; x++)
+		printf(x + '0');
+	
+	return (0);
+}
