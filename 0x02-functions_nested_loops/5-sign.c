28ab1a2d8c0ef65a7da8455f7222cd161511bfc0
kfsibanyoni <kfsibanyoni@gmail.com>
2023-04-25 13:46:11 -0700

    sign of n

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - print (+): n > 0; (0): n = 0; (-): n < 0
+ * @n: input value
+ * return: 1 = (+); 0 = (0); -1 = (-)
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	_putchar('\n');
+}
