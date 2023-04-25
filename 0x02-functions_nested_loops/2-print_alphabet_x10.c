7d773ff9dcb4455f4d590c88af76e1d5ef4db6dc
kfsibanyoni <kfsibanyoni@gmail.com>
2023-04-25 12:35:51 -0700

    putchar w\in parent loop

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,7 +4,7 @@
 /**
  * print_alphabet_x10 - print alphabet in lower case x10
  *
- * void: returns 0 
+ * void: returns 0
  *
  */
 void print_alphabet_x10(void)
@@ -14,13 +14,13 @@ void print_alphabet_x10(void)
 	while (i < 10)
 	{
 		char  alphabet = 'a';
-		
+
 		while (alphabet <= 'z')
 		{
 			_putchar(alphabet);
 			alphabet++;
 		}
 		i++;
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
