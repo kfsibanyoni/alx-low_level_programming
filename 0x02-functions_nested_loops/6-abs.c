dd5cf354dcd157379870c4d959217c17cddffd89
kfsibanyoni <kfsibanyoni@gmail.com>
2023-04-25 15:15:26 -0700

    maybe abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * int _abs - find abs value of a n integer
+ *
+ * return: abs value
+ *
+ */
+int _abs(int)
+{
+	if (int >= 0)
+	{
+		return (int);
+	}
+	else if (int < 0)
+	{
+		return (int * -1);
+	}
+	_putchar('\n');
+}
