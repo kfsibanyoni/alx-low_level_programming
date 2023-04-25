8654048b329d8966fcfd458445c03c7d237342b1
kfsibanyoni <kfsibanyoni@gmail.com>
2023-04-25 10:24:35 -0700

    main not working

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,12 +2,21 @@
 #include "main.h"
 
 /**
- * main - entry point
- *
- * Return: 0 always
+ * main - Entry point.
+ * Description: write a program that prints "_putchar" followed by a new line.
+ * Return: Always 0 (Success.)
  */
- int main(void)
- {
-	printf("_putchar\n");
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
 	return (0);
- }
+}
