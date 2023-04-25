72fece618bb25a868a7b651273b3c6ff23afff6e
kfsibanyoni <kfsibanyoni@gmail.com>
2023-04-25 13:24:00 -0700

    incorrect upper case condition and betty doc

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * _islower - return 1 if character is lower case, else 0
+ * _isalpha - return 1 if character is a letter, else 0
  * @c: formal parameter
  * Return: 1(success); 0(false)
  *
  */
 int _isalpha(int c)
 {
-	if ((c >= 97 && c <= 122) || (c <= 65 && c >= 90))
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 	{
 		return (1);
 	}
