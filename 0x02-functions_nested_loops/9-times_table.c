22f4bfe8b954725cd177fc8cb3ea3aa8906276de
kfsibanyoni <kfsibanyoni@gmail.com>
2023-04-26 03:15:21 -0700

    9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * times_table - print 9 times table starting w\ 0
+ *
+ * Return: 0(success)
+ */
+void times_table(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		int j;
+	
+		for (j = 0; j <= 9; j--)
+		{
+			return (j);
+			_putchar('=');
+		}
+		return (i);
+		_putchar('*');
+	}
+	return (i * j);
+}
