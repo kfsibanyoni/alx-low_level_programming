46943ae93c312254ce4e4e495321565d2cadf7e7
kfsibanyoni <kfsibanyoni@gmail.com>
2023-04-28 10:46:53 -0700

    Commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -7,15 +7,16 @@
  *
  * return: 1(success), 0(fail)
  */
-
 int _isupper(int c)
 {
-	if (c >= 65 && c <= 90){
+	if (c >= 65 && c <= 90)
+	{
 		return (1);
 	}
 	else
 	{
 		return (0);
 	}
+
 	_putchar('\n');
 }
