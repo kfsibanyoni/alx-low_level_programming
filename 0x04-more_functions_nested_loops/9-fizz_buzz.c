fe8a0f90214746ed5876a8469e716404cd44a55e
kfsibanyoni <kfsibanyoni@gmail.com>
2023-04-28 19:14:23 -0700

    buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+int main(void)
+{
+	int x;
+
+	for (x = 1; x < 100; x++)
+	{
+		if (x % 3 == 0)
+		{
+			printf("%s", "Fizz");
+		}
+		if (x % 5 == 0)
+		{
+			printf("%s", "Buzz");
+		}
+		else if (x % 3 == 0 && x % 5 == 0)
+		{
+			printf("%s", "FizzBuzz");
+		}
+		else 
+		{
+			printf("%d", i);
+		}
+		if (i != 100)
+		{
+			printf(" ");
+		}
+	}
+		printf("\n");
+}
