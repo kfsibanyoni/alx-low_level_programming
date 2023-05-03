117585739c37c0f05698693b3d9fbae3943f01f0
kfsibanyoni <kfsibanyoni@gmail.com>
2023-05-02 18:22:02 -0700

    7 puts half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ *
+ * @str: string pointer
+ *
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int i, n;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+	if (i % 3 == 0)
+	{
+		n = ((i - 1) / 2) + 1;
+		for (; n < i; n++)
+		{
+			_putchar(str[n]);
+		}
+	}
+	else
+	{
+		n = i / 2;
+		for (; n < i; n++)
+		{
+			_putchar(str[n]);
+		}
+	}
+
+	_putchar('\n');
+}
