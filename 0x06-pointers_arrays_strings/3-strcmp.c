9228c8d27b506e713c62f6e64aadf154bb14dfb5
kfsibanyoni <kfsibanyoni@gmail.com>
2023-05-04 21:26:56 +0200

    ???

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,24 +11,20 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i, j;
+	int i = 0;
 
-	while (s1[i] != '\0')
+	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0')
 	{
 		i++;
 	}
 
-	while (s2[j] != '\0')
+	if (s1[i] == s2[i])
 	{
-		j++;
+		return (0);
+	}
+	else
+	{
+		return (s1[i] - s2[i]);
 	}
 
-	if (i < j)
-		return (*s1 - *s2);
-	else if (i > j)
-		return (*s1 - *s2);
-	else if (i == j)
-		return (0);
-	
-	return (i);
 }
