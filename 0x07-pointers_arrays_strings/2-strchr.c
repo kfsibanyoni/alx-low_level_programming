c06020faa1cd96c5773e0bcaa507d192e87f7380
kfsibanyoni <kfsibanyoni@gmail.com>
2023-05-09 02:12:15 +0200

    again

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,16 +11,12 @@
 
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
+	while (*s != c)
 	{
-		if (*s == c)
-		{
-			return (s);
-		}
-		else
+		if (!*s++)
 			return (NULL);
 
-		++s;
+		return (*s);
 	}
 	return (0);
 }
