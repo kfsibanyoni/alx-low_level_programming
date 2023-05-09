b6cfc98dca1d6d666aba4eae2ddaa760600c95cf
kfsibanyoni <kfsibanyoni@gmail.com>
2023-05-09 02:18:56 +0200

    bracing

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -16,13 +16,14 @@ char *_strchr(char *s, char c)
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == c)
+		{
 			s += i;
-		return (s);
-	}
-	if (c == '\0')
-	{
-		return (s + i);
-	}
+			return (s);
+		}
+		if (c == '\0')
+		{
+			return (s + i);
+		}
 
 	return (0);
 }
