4bcc06e84a80adf25e88023b84f8dfdfa8bdb672
kfsibanyoni <kfsibanyoni@gmail.com>
2023-05-10 17:06:41 +0200

    recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -10,8 +10,10 @@
 
 int _strlen_recursion(char *s)
 {
+	/* s = " "*/
 	if (*s == 0)
+		/* stop counter*/
 		return (0);
-	
+	/* strlen("k") = strlen ("") + 1 = 1 etc*/
 	return (_strlen_recursion(s + 1) + 1);
 }
---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * factorial - factorial of given number
+ *
+ * @n: argument
+ *
+ * Return: n < 0 -> -1(error)
+ * factorial(0): 1
+ */
+
+int factorial(int n)
+{
+        if (n < 0)
+                return (-1);
+	if (n == 0)
+		return (1);
+
+       return (n * factorial(n - 1));
+
+}
