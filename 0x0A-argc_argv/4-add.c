d5a8882777486111c4ef77e03f30fb0a3fd7290e
kfsibanyoni <kfsibanyoni@gmail.com>
2023-05-12 21:56:16 +0200

    arguments

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+/**
+ * main - adds positive numbers
+ *
+ * @argc: number of args
+ * @argv: strings
+ *
+ * Return: sum of pos ints, 0 if no number, "Error"(1) if not digit
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j;
+	int sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+
+		sum = 0 + atoi(argv[i]);
+	}
+
+	printf("%d\n", sum);
+	return (0);
+}
