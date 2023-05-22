7c10ac898fd1d5c6179406c8b4e53f04b91b155e
kfsibanyoni <kfsibanyoni@gmail.com>
2023-05-22 23:42:24 +0200

    first struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - new type
+ *
+ * @name: pointer to name of dog
+ * @age: age of dog
+ * @owner: pointer to owner info
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
