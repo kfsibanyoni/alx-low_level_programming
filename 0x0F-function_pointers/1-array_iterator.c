78fed25c17b52e1daffddd72c67f6dd3aac72592
kfsibanyoni <kfsibanyoni@gmail.com>
2023-05-24 19:31:01 +0200

    do action per element in []

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes a fucntion given as a parameter on ea. element of []
+ *
+ * @array: pointer to array
+ * @size: size of the array
+ * @action: pointer to function to use
+ *
+ * Return: void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int x; /* counter */
+
+	/* fails */
+	if (array == NULL || action == NULL)
+		return;
+
+	/* execute function over ea. element */
+	x = 0;
+	while (x < (int) size)
+	{
+		action(array[x]);
+	}
+}
