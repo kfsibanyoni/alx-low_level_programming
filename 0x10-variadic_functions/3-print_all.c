355670e4ff2fab6343dc69aa5b17da7c82f1aca5
kfsibanyoni <kfsibanyoni@gmail.com>
2023-05-25 20:44:17 +0200

    description of param format

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -6,7 +6,7 @@
 /**
  * print_all - prints anything
  *
- * @:format: list of types of arguments passed to the function
+ * @format: list of types of arguments passed to the function
  *
  * Return: void
  */
