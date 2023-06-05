982b3ccdc141a50f25127c44d958570af0098f4d
kfsibanyoni <kfsibanyoni@gmail.com>
2023-06-05 22:44:18 +0200

    another free list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t list
+ *
+ * @head: double ptr to starting node
+ *
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t **tmp = head;
+	listint_t *id;
+
+	if (tmp != NULL)
+	{
+		while (*head)
+		{
+			id = *head;
+				free(id);
+
+			*head = (*head)->next;
+		}
+
+		*tmp = NULL;
+	}
+}			
