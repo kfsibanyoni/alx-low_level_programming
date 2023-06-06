0dfea395b608e9ff457527f09a17d07893e61dc3
kfsibanyoni <kfsibanyoni@gmail.com>
2023-06-06 21:30:37 +0200

    betty on get node (7)

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,8 +1,9 @@
 #include "lists.h"
 
 /**
- * get_nodeit_at_index - returns the nth node of a listint_t linked list
- * @head
+ * get_nodeint_at_index - returns the nth node of a listint_t linked list
+ *
+ * @head: start node
  * @index: index of the node, starting at 0
  *
  * Return: NULL if node does not exist
