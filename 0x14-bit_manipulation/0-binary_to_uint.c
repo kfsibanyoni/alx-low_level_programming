3a165de92f25ab6750643b2cafbad7cf17f7805c
kfsibanyoni <kfsibanyoni@gmail.com>
2023-06-08 22:26:36 +0200

    betty and for loop change

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -18,11 +18,11 @@ unsigned int binary_to_uint(const char *b)
 	if (b == NULL)
 		return (0);
 
-       	for (i = 0; b[i] != 0; i++)
+	for (i = 0; b[i]; i++)
 	{
 		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		
+
 		x = 2 * x + (b[i] - '0');
 	}
 
