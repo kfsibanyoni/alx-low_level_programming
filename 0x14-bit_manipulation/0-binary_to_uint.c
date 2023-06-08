57bc4faa248443773e9f3c6ae7115d733713be72
kfsibanyoni <kfsibanyoni@gmail.com>
2023-06-08 21:44:40 +0200

    trailing whitespace and function description

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * binary_to_unit - converts a binary number to an unsigned int
+ * binary_to_uint - converts a binary number to an unsigned int
  *
  * @b: points to a string of 0 and 1 chars
  *
- * Return: the converted number, 
+ * Return: the converted number,
  * 0 if: there is one or more chars in the string b that is not 0 or 1
  * b is NULL
  */
