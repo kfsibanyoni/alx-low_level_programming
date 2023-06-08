070e82c317d680c86d3c20a112a2fdba3ab1f401
kfsibanyoni <kfsibanyoni@gmail.com>
2023-06-08 22:23:37 +0200

    corrected condtions

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -18,10 +18,12 @@ unsigned int binary_to_uint(const char *b)
 	if (b == NULL)
 		return (0);
 
-	for (i = 0; b[i] == '0' || b[i] == '1'; i++)
+       	for (i = 0; b[i] != 0; i++)
 	{
-		x <<= 1;
-		x += b[i] - '0';
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		
+		x = 2 * x + (b[i] - '0');
 	}
 
 	return (x);
