6d6fa42e559fe9b4bacf185b604ecd37e71a0d08
kfsibanyoni <kfsibanyoni@gmail.com>
2023-06-12 18:22:58 +0200

    removed w

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -32,7 +32,7 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	f = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(f, text_content, n);
+	write(f, text_content, n);
 
 	if (f < 0)
 		return (-1);
