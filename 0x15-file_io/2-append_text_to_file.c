6feb7355903c9158e1a3a2658c0372ab1a4faf3a
kfsibanyoni <kfsibanyoni@gmail.com>
2023-06-12 18:51:20 +0200

    betty go bye

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * append_text_to_file(const char *filename, char *text_content);
+ * append_text_to_file - appends text at the end of a file;
  *
  * @filename: name of the file
  * @text_content: NULL terminated string to add at the end of the file
@@ -11,7 +11,7 @@
  * If filename is NULL return -1
  * If text_content is NULL, do not add anything to the file.
  * 1 if the file exists
- * -1 if the file does not exist 
+ * -1 if the file does not exist
  * or if you do not have the required permissions to write the file
  */
 
