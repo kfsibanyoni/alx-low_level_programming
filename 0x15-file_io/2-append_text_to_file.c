8e652a7cea647c43bbbd38878cdfaa72a4815c06
kfsibanyoni <kfsibanyoni@gmail.com>
2023-06-12 18:43:36 +0200

    ammended text content in for loop

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -27,7 +27,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	f = open(filename, O_WRONLY | O_APPEND);
 
-	for (n = 0; *text_content != '\0'; n++)
+	for (n = 0; text_content[n] != '\0'; n++)
 		;
 
 	if (f < 0)
