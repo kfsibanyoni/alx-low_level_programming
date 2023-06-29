9c52a2f0ce644a31d1bbbf6528cb425802fe3b2e
kfsibanyoni <kfsibanyoni@gmail.com>
2023-06-29 21:38:16 +0200

    cp from one file to another

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,10 +1,14 @@
 #ifndef _MAIN_H
 #define _MAIN_H
 
+#define BUFSIZE 1024
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+
+
 
 /* cutomised */
 int _putchar(char c);
