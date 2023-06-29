24544af7c411c6f242ee691d8a92e68da5417ccd
kfsibanyoni <kfsibanyoni@gmail.com>
2023-06-29 21:59:25 +0200

    modified bit clearing

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -14,7 +14,10 @@ int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (n == NULL || index > 64)
 		return (-1);
-
-	*n &= ~(index >> 1);
+	if ((*n >> index) & 1)
+	{
+		*n -= 1 << index;
+		return (1);
+	}
 	return (1);
 }
