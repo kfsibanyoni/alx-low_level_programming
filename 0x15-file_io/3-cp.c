d5ec0fb1ad34db5d0ab52fcb43720c67271e00f4
kfsibanyoni <kfsibanyoni@gmail.com>
2023-07-12 12:10:25 +0200

    betty

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -34,30 +34,25 @@ void _close(int fd)
  * > dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2])
  * exit(100): if you can not close a file descriptor
  * > dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd)
- * Permissions of the created file: rw-rw-r--. 
+ * Permissions of the created file: rw-rw-r--.
  * > If the file already exists, do not change the permissions
  * Must read 1,024 bytes at a time from the file_from. Use a buffer
  */
 int main(int ac, char *av[])
 {
 	int from, to, rd, wr;
-	char *buf;
+	char *buf = malloc(sizeof(char) * 1024);
 
-	/* number of arguments is not the correct */
 	if (ac != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
-	buf = malloc(sizeof(char) * 1024);
-	/* cannot create or write to file_to fails */
 	if (buf == NULL)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		exit(99);
 	}
-
 	from = open(av[1], O_RDONLY);
 	to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	rd = read(from, buf, 1024);
@@ -66,7 +61,6 @@ int main(int ac, char *av[])
 	{
 		if (from == -1 || rd == -1)
 		{
-			/* file_from does not exist, or cannot be read */
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 			free(buf);
 			exit(98);
@@ -76,17 +70,14 @@ int main(int ac, char *av[])
 		if (to == -1 || wr == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-                free(buf);
+		free(buf);
 		exit(99);
 		}
-
 		rd = read(from, buf, 1024);
 		to = open(av[2], O_WRONLY | O_APPEND);
 	}
-
 	free(buf);
 	_close(from);
 	_close(to);
-
 	return (0);
 }
